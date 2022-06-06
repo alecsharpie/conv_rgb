47a6ae170197c12021b5604527fd5e0ce07b9c5b
alec sharp <alecsharpie@gmail.com>
2022-06-06 14:25:10 +1000

    add optional rescaling layer, add doc strings, tidt imports and requirements

---
+++ build/lib/tests/model_test.py
@@ -0,0 +1,10 @@
+from conv_rbg.models import ConvRGB
+
+
+def test_create_model():
+    """
+    GIVEN ConvRGB with
+    WHEN the class is instantiated is called
+    THEN a new Article must exist in the database with the same attributes
+    """
+    assert True
